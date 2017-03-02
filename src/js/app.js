edbe1947a3b632bbfbdf9c9a2de159a4d5e7b094
BaQudo <baqudo@gmail.com>
2017-03-02 17:01:18 +0200

    main>content+sidebar

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,34 @@
 //=include lib/svgxuse.min.js
 
 sayHello();
+
+$(function () {
+	var $filter = $('.js-filter');
+	var duration = 300;
+
+	$filter.each(function () {
+		var
+				$this = $(this),
+				$button = $this.children('.js-filter__button'),
+				$dropdown = $this.children('.js-filter__dropdown'),
+				$label = $dropdown.children('.js-filter__label'),
+				$checkbox = $label.children('.js-filter__checkbox');
+
+			$button.on('click', toggleList);
+			$checkbox.on('click', changeActivity);
+	})
+
+	function toggleList() {
+		var $this = $(this);
+		
+		$this.next('.js-filter__dropdown').slideToggle(duration);
+	}
+
+	function changeActivity() {
+		var
+				$this = $(this),
+				$parentLabel = $this.parent();
+
+		$parentLabel.toggleClass('is-checked');
+	}
+});
